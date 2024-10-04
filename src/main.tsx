73d2e11902be387870fe932b1f0f1a1792cf0057
Lucas Bastos Pirolli <lucaspir@marcopolo.com.br>
2024-10-04 10:16:17 -0300

    feat: layout home done and routers link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
+// Componentes React
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Libs Components
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Páginas TrackFit
 import Home from "./modules/pages/Home.tsx";
+
+// Estilos TrackFit
 import "./styles/index.scss";
+
+const Main = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <Main />
   </React.StrictMode>
 );
--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -1,7 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import "../../styles/pages/home.scss";
 
 const Home = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <section className="home">
+        <h1 className="logo">TrackFit</h1>
+        <button className="btn-access" onClick={() => navigate("/login")}>
+          Iniciar sessão
+        </button>
+
+        <span className="new-user">
+          é um novo utilizador do TrackFit?{" "}
+          <a
+            className="link"
+            onClick={() => {
+              navigate("/register");
+            }}
+          >
+            Registrar
+          </a>
+        </span>
+      </section>
+    </>
+  );
 };
 
 export default Home;
